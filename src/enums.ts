54805d92a4ebb5cb3dc1c963e0a7a21de4700c7e
Lexx-Dev <alexcalle.paola@gmail.com>
2023-02-15 10:18:21 -0500

    add types and enums

---
+++ src/enums.ts
@@ -0,0 +1,10 @@
+export enum ResponseType {
+  JSON = 'json',
+  TEXT = 'text'
+}
+
+export enum animeStatus {
+  BROADCAST = '1',
+  FINALIZED = '2',
+  SOON = '3'
+}
---
+++ src/types.d.ts
@@ -0,0 +1,59 @@
+interface EpisodeBase {
+  orgLink?: string
+  title?: string
+}
+
+export interface Episode extends EpisodeBase {
+  episode: number
+}
+
+export interface LastEpisode extends EpisodeBase {
+  image?: string
+  episode?: string
+}
+
+export interface EpisodeVideo {
+  server: string
+  title: string
+  ads: number
+  url?: string
+  allow_mobile: boolean
+  code: string
+}
+
+export interface VideoList {
+  SUB?: EpisodeVideo[]
+  DUB?: EpisodeVideo[]
+}
+
+export interface EpisodeSources {
+  animeId: string
+  episodeId: string
+  episode: string
+  videos: VideoList
+}
+
+interface AnimeBase {
+  image?: string
+  title?: string
+  type?: string
+  rank?: string
+}
+
+export interface Anime extends AnimeBase {
+  description?: string
+  genres: string[]
+  related: string[]
+  episodes: Episode[]
+}
+
+export interface ShortAnime extends AnimeBase {
+  originalLink?: string
+  shortDescription?: string
+}
+
+export interface EmisionAnime extends AnimeBase {
+  originalLink?: string
+  title?: string
+  type?: string
+}
