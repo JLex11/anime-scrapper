c02dd96bf833f9c98a945d116a07b8c76cd3b139
Lexx-Dev <alexcalle.paola@gmail.com>
2023-02-15 10:31:20 -0500

    add optional assigment for PORT

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,7 @@ import express from 'express'
 import router from './router/routes'
 
 const app = express()
-const PORT = 3001
+const PORT = process.env.PORT ?? 3001
 
 app.use(express.json())
 app.use(router)
