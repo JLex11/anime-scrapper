536011fcb659fd56766988133c62621e089df206
Lexx-Dev <alexcalle.paola@gmail.com>
2023-02-15 10:18:50 -0500

    add express app

---
+++ src/index.ts
@@ -0,0 +1,13 @@
+import c from 'colors/safe'
+import express from 'express'
+import router from './router/routes'
+
+const app = express()
+const PORT = 3001
+
+app.use(express.json())
+app.use(router)
+
+app.listen(PORT, () => {
+  console.log(`🚀 Server running on ${c.blue(`http://localhost:${PORT}`)} 🚀`)
+})
