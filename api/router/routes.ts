1158f64f81ddf2d4722e617e2e926c7d5d5f0721
Lexx-Dev <alexcalle.paola@gmail.com>
2023-02-24 21:25:50 -0500

    add to all routes api path

--- api/router/routes.ts
+++ api/router/routes.ts
@@ -6,31 +6,31 @@ import routesDocumentation from './routesDocumentation'
 
 const router = Router()
 
-router.get('/', (_, res) => res.send(routesDocumentation))
+router.get('/api/', (_, res) => res.send(routesDocumentation))
 
-router.get('/episodes/latest', async (_, res) => {
+router.get('/api/episodes/latest', async (_, res) => {
   const latestEpisodes = await scrapeLastEpisodes()
   return res.send(latestEpisodes)
 })
 
-router.get('/episodes/sources/:id', async (req, res) => {
+router.get('/api/episodes/sources/:id', async (req, res) => {
   const { id } = req.params
 
   const emisionAnimes = await scrapeEpisodeSources(id)
   return res.send(emisionAnimes)
 })
 
-router.get('/animes/latest', async (_, res) => {
+router.get('/api/animes/latest', async (_, res) => {
   const latestAnimes = await scrapeLastAnimes()
   return res.send(latestAnimes)
 })
 
-router.get('/animes/broadcast', async (_, res) => {
+router.get('/api/animes/broadcast', async (_, res) => {
   const emisionAnimes = await scrapeEmisionAnimes()
   return res.send(emisionAnimes)
 })
 
-router.get('/animes/latest/rating', async (_, res) => {
+router.get('/api/animes/latest/rating', async (_, res) => {
   const ratingAnimes = await scrapeRatingAnimes(animeStatus.BROADCAST)
   return res.send(ratingAnimes)
 })
