ce6325b9f1c0f8b41f3c3b8353045c0dfa989f07
Lexx-Dev <alexcalle.paola@gmail.com>
2023-03-25 20:59:43 -0500

    add cors and bit refactor to index app

--- api/index.ts
+++ api/index.ts
@@ -1,14 +1,38 @@
+import cors from 'cors'
+import * as dotenv from 'dotenv'
 import express from 'express'
-import router from './router/routes'
+import { getOriginPath, setOriginPath } from './config'
+import animesRouter from './router/animes'
+import episodesRouter from './router/episodes'
+import routesDocumentation from './router/routesDocumentation'
+import { mapOriginPath } from './utils/mapOriginPath'
+dotenv.config()
 
 const app = express()
 
+app.use(cors())
 app.use(express.json())
 app.use(express.static('public'))
-app.use('/api', router)
+
+app.use((req, _, next) => {
+  setOriginPath(`${req.protocol}://${req.get('host')}`)
+  next()
+})
+
+app.get('/api', (_, res) => {
+  const originPath = getOriginPath()
+
+  return res.send(routesDocumentation.map(docRoute => ({
+    ...docRoute,
+    route: mapOriginPath(originPath, `api${docRoute.route}`)
+  })))
+})
+
+app.use('/api', animesRouter)
+app.use('/api', episodesRouter)
 
 if (process.env.NODE_ENV !== 'production') {
   app.listen(3001, () => console.log('Server running on port 3001'))
 }
-
+ 
 export default app
