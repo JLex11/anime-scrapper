8409993f6a9f7c280613197bc983eaad0ef3b1ec
Lexx-Dev <alexcalle.paola@gmail.com>
2023-04-20 08:06:33 -0500

    refactor

--- api/router/animes.ts
+++ api/router/animes.ts
@@ -1,23 +1,34 @@
 import { Router } from 'express'
-import { scrapeFoundAnimes, scrapeLastAnimes } from '../scrapers/animes'
-import { endPoints } from './../enums'
+import {
+  scrapeEmisionAnimes,
+  scrapeFoundAnimes,
+  scrapeFullAnimeInfo,
+  scrapeLastAnimes,
+  scrapeRatingAnimes,
+} from '../scrapers/animes'
+import { Anime } from '../types'
+import { animeStatus, endPoints } from './../enums'
 
 const router = Router()
 
-router.get(endPoints.LATEST_ANIMES, async (_, res) => {
-  const latestAnimes = await scrapeLastAnimes()
+router.get(endPoints.LATEST_ANIMES, async (req, res) => {
+  const { limit } = req.query
+
+  const latestAnimes = await scrapeLastAnimes(Number(limit))
   return res.send(latestAnimes)
 })
 
-/* router.get(endPoints.BROADCAST_ANIMES, async (_, res) => {
+router.get(endPoints.BROADCAST_ANIMES, async (_, res) => {
   const emisionAnimes = await scrapeEmisionAnimes()
   return res.send(emisionAnimes)
-}) */
+})
 
-/* router.get(endPoints.RATING_ANIMES, async (_, res) => {
-  const ratingAnimes = await scrapeRatingAnimes(animeStatus.BROADCAST)
+router.get(endPoints.RATING_ANIMES, async (req, res) => {
+  const { limit } = req.query
+
+  const ratingAnimes = await scrapeRatingAnimes(animeStatus.BROADCAST, Number(limit))
   return res.send(ratingAnimes)
-}) */
+})
 
 router.get(endPoints.SEARCH_ANIMES, async (req, res) => {
   const { query } = req.params
@@ -26,11 +37,11 @@ router.get(endPoints.SEARCH_ANIMES, async (req, res) => {
   return res.send(foundAnimes)
 })
 
-/* router.get(endPoints.ANIME_INFO, async (req, res) => {
+router.get(endPoints.ANIME_INFO, async (req, res) => {
   const { animeId } = req.params
 
   const foundAnime: Anime = await scrapeFullAnimeInfo(animeId)
   return res.send(foundAnime)
-}) */
+})
 
 export default router
