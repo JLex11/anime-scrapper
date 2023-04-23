f004cab703d0675d30f84412a9bd6a79661fd9a9
Lexx-Dev <alexcalle.paola@gmail.com>
2023-04-23 13:09:17 -0500

    change .env variable names to issue with netlifly

--- api/services/awsS3Service.ts
+++ api/services/awsS3Service.ts
@@ -2,13 +2,16 @@ import { HeadObjectCommand, PutObjectCommand, S3Client } from '@aws-sdk/client-s
 import * as dotenv from 'dotenv'
 dotenv.config()
 
-const { AWS_ACCESS_KEY_ID, AWS_SECRET_ACCESS_KEY, AWS_S3_REGION, AWS_S3_BUCKET } = process.env
+const MY_AWS_ACCESS_KEY_ID = process.env.MY_AWS_ACCESS_KEY_ID
+const MY_AWS_SECRET_ACCESS_KEY = process.env.MY_AWS_SECRET_ACCESS_KEY
+const MY_AWS_S3_REGION = process.env.MY_AWS_S3_REGION
+const MY_AWS_S3_BUCKET = process.env.MY_AWS_S3_BUCKET
 
 const client = new S3Client({
-  region: AWS_S3_REGION,
+  region: MY_AWS_S3_REGION,
   credentials: {
-    accessKeyId: AWS_ACCESS_KEY_ID!,
-    secretAccessKey: AWS_SECRET_ACCESS_KEY!,
+    accessKeyId: MY_AWS_ACCESS_KEY_ID!,
+    secretAccessKey: MY_AWS_SECRET_ACCESS_KEY!,
   },
 })
 
@@ -30,24 +33,30 @@ export async function s3Request({ operation, fileName, fileBuffer }: S3Request) 
   return
 }
 
-async function s3PutOperation({ fileName, fileBuffer }: { fileName: string, fileBuffer: Buffer}) {
+async function s3PutOperation({
+  fileName,
+  fileBuffer,
+}: {
+  fileName: string
+  fileBuffer: Buffer
+}) {
   const command = new PutObjectCommand({
-    Bucket: AWS_S3_BUCKET,
+    Bucket: MY_AWS_S3_BUCKET,
     Key: fileName,
     Body: fileBuffer,
     ContentLength: fileBuffer.length,
   })
 
   await client.send(command)
-  return `https://${AWS_S3_BUCKET}.s3.${AWS_S3_REGION}.amazonaws.com/${fileName}`
+  return `https://${MY_AWS_S3_BUCKET}.s3.${MY_AWS_S3_REGION}.amazonaws.com/${fileName}`
 }
 
 async function s3GetOperation({ fileName }: { fileName: string }) {
   const command = new HeadObjectCommand({
-    Bucket: AWS_S3_BUCKET!,
+    Bucket: MY_AWS_S3_BUCKET!,
     Key: fileName,
   })
-  
+
   /* const command = new GetObjectCommand({
     Bucket: AWS_S3_BUCKET!,
     Key: fileName,
@@ -55,7 +64,7 @@ async function s3GetOperation({ fileName }: { fileName: string }) {
 
   try {
     await client.send(command)
-    return `https://${AWS_S3_BUCKET}.s3.${AWS_S3_REGION}.amazonaws.com/${fileName}`
+    return `https://${MY_AWS_S3_BUCKET}.s3.${MY_AWS_S3_REGION}.amazonaws.com/${fileName}`
   } catch (error) {
     console.error(error)
     return
