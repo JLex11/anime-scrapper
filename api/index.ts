510cab94e247cba6e7c47ca6a9b4498c0c31bdcb
Lexx-Dev <alexcalle.paola@gmail.com>
2023-05-11 09:36:55 -0500

    add morgan to see request logs

--- api/index.ts
+++ api/index.ts
@@ -1,6 +1,7 @@
 import cors from 'cors'
 import * as dotenv from 'dotenv'
 import express from 'express'
+import morgan from 'morgan'
 import { setOriginPath } from './config'
 import animesRouter from './router/animes'
 import episodesRouter from './router/episodes'
@@ -14,6 +15,7 @@ const app = express()
 app.use(cors())
 app.use(express.json())
 app.use(express.static('public'))
+app.use(morgan('dev'))
 
 app.use((req, _, next) => {
   setOriginPath(`${req.protocol}://${req.get('host')}`)
