51e70d817da63872f4869a8df10c564660fee758
Lexx-Dev <alexcalle.paola@gmail.com>
2023-05-11 09:38:33 -0500

    search by query in database

---
+++ api/controllers/animes/searchAnimes.ts
@@ -0,0 +1,8 @@
+import { getAnimesByQuery } from '../../services/database/animes'
+
+export const searchAnimes = async (query: string, limit?: number) => {
+  const animesResponse = await getAnimesByQuery(query, limit || 30)
+
+  const animes = animesResponse.data
+  return animes
+}
