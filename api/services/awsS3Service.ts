a3e6565d2961226b39464077a996ac27987bb95a
Lexx-Dev <alexcalle.paola@gmail.com>
2023-05-20 09:05:34 -0500

    change aws s3 to use api rest

--- api/services/awsS3Service.ts
+++ api/services/awsS3Service.ts
@@ -1,4 +1,4 @@
-import { HeadObjectCommand, PutObjectCommand, S3Client } from '@aws-sdk/client-s3'
+/* import { S3Client } from '@aws-sdk/client-s3' */
 import * as dotenv from 'dotenv'
 dotenv.config()
 
@@ -7,44 +7,13 @@ const MY_AWS_SECRET_ACCESS_KEY = 'L1u8ONJrEph8vQTaFNgel76cNGvYX1dU+ZJZ+1lf' //pr
 const MY_AWS_S3_BUCKET = 'anime-app' //process.env.MY_AWS_S3_BUCKET
 const MY_AWS_S3_REGION = 'us-east-1' //process.env.MY_AWS_S3_REGION
 
-const client = new S3Client({
+/* const client = new S3Client({
   region: MY_AWS_S3_REGION,
   credentials: {
     accessKeyId: MY_AWS_ACCESS_KEY_ID!,
     secretAccessKey: MY_AWS_SECRET_ACCESS_KEY!,
   },
-})
-
-/*
-
-const authorization = MY_AWS_ACCESS_KEY_ID;
-
-async function fetchImage() {
-  const url = `https://${MY_AWS_S3_BUCKET}.s3.${MY_AWS_S3_REGION}.amazonaws.com/${fileName}`;
-  const headers = new Headers();
-  headers.append('Host', bucketName);
-  headers.append('Date', new Date().toUTCString());
-  headers.append('x-amz-date', new Date().toUTCString());
-  headers.append('Authorization', authorization);
-
-  const response = await fetch(url, {
-    method: 'GET',
-    headers: headers,
-  });
-
-  if (response.status === 200) {
-    const imageData = await response.blob();
-
-    // Do something with the image data...
-  } else {
-    console.log('Error fetching image:', response.status);
-  }
-}
-
-fetchImage();
-
-
-*/
+}) */
 
 interface S3Request {
   operation: 'putObject' | 'getObject'
@@ -70,7 +39,7 @@ interface S3PutOperation {
 }
 
 async function s3PutOperation({ fileName, fileBuffer }: S3PutOperation) {
-  const command = new PutObjectCommand({
+  /* const command = new PutObjectCommand({
     Bucket: MY_AWS_S3_BUCKET,
     Key: fileName,
     Body: fileBuffer,
@@ -78,11 +47,26 @@ async function s3PutOperation({ fileName, fileBuffer }: S3PutOperation) {
   })
 
   await client.send(command)
+  return `https://${MY_AWS_S3_BUCKET}.s3.${MY_AWS_S3_REGION}.amazonaws.com/${fileName}` */
+
+  const blob = new Blob([fileBuffer])
+
+  const request = new Request(`https://s3.amazonaws.com/${MY_AWS_S3_BUCKET}/${fileName}`, {
+    method: 'PUT',
+    body: blob,
+    headers: {
+      Authorization: `Bearer ${MY_AWS_ACCESS_KEY_ID}:${MY_AWS_SECRET_ACCESS_KEY}`,
+    },
+  })
+
+  const response = await fetch(request)
+  console.log(response)
+
   return `https://${MY_AWS_S3_BUCKET}.s3.${MY_AWS_S3_REGION}.amazonaws.com/${fileName}`
 }
 
 async function s3GetOperation({ fileName }: { fileName: string }) {
-  const command = new HeadObjectCommand({
+  /* const command = new HeadObjectCommand({
     Bucket: MY_AWS_S3_BUCKET!,
     Key: fileName,
   })
@@ -92,5 +76,18 @@ async function s3GetOperation({ fileName }: { fileName: string }) {
     return `https://${MY_AWS_S3_BUCKET}.s3.${MY_AWS_S3_REGION}.amazonaws.com/${fileName}`
   } catch (error) {
     return
-  }
+  } */
+
+  const request = new Request(`https://s3.amazonaws.com/${MY_AWS_S3_BUCKET}/${fileName}`, {
+    method: 'HEAD',
+  })
+
+  await fetch(request)
+    .then(response => {
+      console.log(response)
+      return `https://${MY_AWS_S3_BUCKET}.s3.${MY_AWS_S3_REGION}.amazonaws.com/${fileName}`
+    })
+    .catch(error => {
+      return
+    })
 }
