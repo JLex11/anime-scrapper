4b3adffe81a8713813f788103484d18c6d7db64f
Lexx-Dev <alexcalle.paola@gmail.com>
2023-05-20 13:56:47 -0500

    use sdk aws s3

--- src/services/awsS3Service.ts
+++ src/services/awsS3Service.ts
@@ -1,4 +1,5 @@
 /* import { S3Client } from '@aws-sdk/client-s3' */
+import { HeadObjectCommand, PutObjectCommand, S3Client } from '@aws-sdk/client-s3'
 import * as dotenv from 'dotenv'
 dotenv.config()
 
@@ -7,13 +8,13 @@ const MY_AWS_SECRET_ACCESS_KEY = 'L1u8ONJrEph8vQTaFNgel76cNGvYX1dU+ZJZ+1lf' //pr
 const MY_AWS_S3_BUCKET = 'anime-app' //process.env.MY_AWS_S3_BUCKET
 const MY_AWS_S3_REGION = 'us-east-1' //process.env.MY_AWS_S3_REGION
 
-/* const client = new S3Client({
+const client = new S3Client({
   region: MY_AWS_S3_REGION,
   credentials: {
     accessKeyId: MY_AWS_ACCESS_KEY_ID!,
     secretAccessKey: MY_AWS_SECRET_ACCESS_KEY!,
   },
-}) */
+})
 
 interface S3Request {
   operation: 'putObject' | 'getObject'
@@ -39,7 +40,7 @@ interface S3PutOperation {
 }
 
 async function s3PutOperation({ fileName, fileBuffer }: S3PutOperation) {
-  /* const command = new PutObjectCommand({
+  const command = new PutObjectCommand({
     Bucket: MY_AWS_S3_BUCKET,
     Key: fileName,
     Body: fileBuffer,
@@ -47,9 +48,9 @@ async function s3PutOperation({ fileName, fileBuffer }: S3PutOperation) {
   })
 
   await client.send(command)
-  return `https://${MY_AWS_S3_BUCKET}.s3.${MY_AWS_S3_REGION}.amazonaws.com/${fileName}` */
+  return `https://${MY_AWS_S3_BUCKET}.s3.${MY_AWS_S3_REGION}.amazonaws.com/${fileName}`
 
-  const blob = new Blob([fileBuffer])
+  /* const blob = new Blob([fileBuffer])
 
   const request = new Request(`https://s3.amazonaws.com/${MY_AWS_S3_BUCKET}/${fileName}`, {
     method: 'PUT',
@@ -62,11 +63,11 @@ async function s3PutOperation({ fileName, fileBuffer }: S3PutOperation) {
   const response = await fetch(request)
   console.log(response)
 
-  return `https://${MY_AWS_S3_BUCKET}.s3.${MY_AWS_S3_REGION}.amazonaws.com/${fileName}`
+  return `https://${MY_AWS_S3_BUCKET}.s3.${MY_AWS_S3_REGION}.amazonaws.com/${fileName}` */
 }
 
 async function s3GetOperation({ fileName }: { fileName: string }): Promise<string | undefined> {
-  /* const command = new HeadObjectCommand({
+  const command = new HeadObjectCommand({
     Bucket: MY_AWS_S3_BUCKET!,
     Key: fileName,
   })
@@ -76,9 +77,9 @@ async function s3GetOperation({ fileName }: { fileName: string }): Promise<strin
     return `https://${MY_AWS_S3_BUCKET}.s3.${MY_AWS_S3_REGION}.amazonaws.com/${fileName}`
   } catch (error) {
     return
-  } */
+  }
 
-  const request = new Request(`https://s3.amazonaws.com/${MY_AWS_S3_BUCKET}/${fileName}`, {
+  /* const request = new Request(`https://s3.amazonaws.com/${MY_AWS_S3_BUCKET}/${fileName}`, {
     method: 'HEAD',
   })
 
@@ -89,5 +90,5 @@ async function s3GetOperation({ fileName }: { fileName: string }): Promise<strin
     })
     .catch(error => {
       return undefined
-    })
+    }) */
 }
