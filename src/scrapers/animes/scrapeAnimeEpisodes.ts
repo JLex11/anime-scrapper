bc150508bda6cfd306d832f525f6e49935c946f1
Lexx-Dev <alexcalle.paola@gmail.com>
2023-05-21 00:28:28 -0500

    fix extration of animeInfo and optimizate episode images

--- src/scrapers/animes/scrapeAnimeEpisodes.ts
+++ src/scrapers/animes/scrapeAnimeEpisodes.ts
@@ -1,9 +1,8 @@
 import { JSDOM } from 'jsdom'
-//import { animeFLVPages } from '../../enums'
-import { requestTextWithCache } from '../../services/requestWithCache'
-//import { Episode } from '../../types.d'
 import { animeFLVPages } from '../../../api/enums'
 import { Episode } from '../../../api/types'
+import { getOptimizeImage } from '../../services/getOptimizeImage'
+import { requestTextWithCache } from '../../services/requestWithCache'
 import { getAnimeTitle } from './animeGetters'
 
 export async function scrapeAnimeEpisodes(animeId: string, offset: number, limit: number): Promise<Episode[]> {
@@ -15,11 +14,11 @@ export async function scrapeAnimeEpisodes(animeId: string, offset: number, limit
   const title = getAnimeTitle(document)
   const scrapedScript = [...document.querySelectorAll('script')].map(s => s.textContent).join(' ')
 
-  let internAnimeId: string | undefined
+  let animeInfo: string | undefined
   let episodesIds: number[][] = []
 
   try {
-    internAnimeId = JSON.parse(
+    animeInfo = JSON.parse(
       scrapedScript
         .split(/(var|let|const) *anime_info *= */)
         .pop()!
@@ -38,20 +37,25 @@ export async function scrapeAnimeEpisodes(animeId: string, offset: number, limit
     return []
   }
 
-  const animeEpisodes = episodesIds.slice(offset || 0, limit || 20).map(([episodeNumber]: number[]): Episode => {
+  const animeEpisodes = episodesIds.slice(offset || 0, limit || 20).map(async ([episodeNumber]: number[]): Promise<Episode> => {
     const episode = episodeNumber
     const episodeId = `${animeId}-${episodeNumber}`
     const originalLink = `${animeFLVPages.BASE}/ver/${episodeId}`
-    const image = `https://cdn.animeflv.net/screenshots/${internAnimeId}/${episodeNumber}/th_3.jpg`
+    const image = `https://cdn.animeflv.net/screenshots/${animeInfo?.[0] ?? 0}/${episodeNumber}/th_3.jpg`
+    const optimizeImage = await getOptimizeImage(image, `episode-image-${animeId}-${episodeNumber}`, {
+      width: 150,
+      height: 80,
+    })
 
     return {
       episodeId,
       title,
       episode,
       originalLink,
-      image,
+      image: optimizeImage || image,
     }
   })
 
-  return animeEpisodes
+  const episodesResults = await Promise.all(animeEpisodes)
+  return episodesResults
 }
