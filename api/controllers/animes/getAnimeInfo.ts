2ff743aeba12cc72609278fd090619d5434cd12e
Lexx-Dev <alexcalle.paola@gmail.com>
2023-08-17 12:21:22 -0500

    fix types and add created_at

--- api/controllers/animes/getAnimeInfo.ts
+++ api/controllers/animes/getAnimeInfo.ts
@@ -1,12 +1,9 @@
 import { scrapeFullAnimeInfo } from '../../../src/scrapers/animes/scrapeFullAnimeInfo'
 import { UpsertAnimes, getAnimeBy } from '../../../src/services/database/animes'
-import { Database } from '../../../src/supabase'
 import { Anime } from '../../../src/types'
 import { isUpToDate } from '../../../src/utils/isUpToDate'
 
-type AnimeInsert = Database['public']['Tables']['animes']['Insert']
-
-export const getAnimeInfo = async (animeId: string) => {
+export const getAnimeInfo = async (animeId: string): Promise<Anime> => {
   const animeInfo = await getAnimeBy('animeId', animeId)
 
   if (animeInfo.data && animeInfo.data.length > 0) {
@@ -21,16 +18,13 @@ export const getAnimeInfo = async (animeId: string) => {
   const currentTime = new Date().toISOString()
 
   const scrapedAnime = await scrapeFullAnimeInfo(animeId)
-  const animeToUpsert: AnimeInsert = {
-    ...scrapedAnime,
-    updated_at: currentTime,
-  }
-  const { data: updatedAnime } = await UpsertAnimes(animeToUpsert)
-  if (updatedAnime?.at(0)) return updatedAnime[0]
-
-  return {
+  const animeToUpsert: Anime = {
     ...scrapedAnime,
     created_at: currentTime,
     updated_at: currentTime,
   }
+  const { data: updatedAnime } = await UpsertAnimes(animeToUpsert)
+  if (updatedAnime?.[0]) return updatedAnime[0]
+
+  return animeToUpsert
 }
