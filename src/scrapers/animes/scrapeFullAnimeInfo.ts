70d14087e572face73d1bd855a66788b52f8e64b
Lexx-Dev <alexcalle.paola@gmail.com>
2023-08-17 12:20:48 -0500

    returns original img link if not can optimize this

--- src/scrapers/animes/scrapeFullAnimeInfo.ts
+++ src/scrapers/animes/scrapeFullAnimeInfo.ts
@@ -4,7 +4,14 @@ import { getCarouselImages } from '../../services/getCarouselImages'
 import { getOptimizeImage } from '../../services/getOptimizeImage'
 import { requestTextWithCache } from '../../services/requestWithCache'
 import { AnimeImages, AnimeWithoutDates } from '../../types'
-import { getAnimeDescription, getAnimeImgLink, getAnimeRank, getAnimeStatus, getAnimeTitle, getAnimeType } from './animeGetters'
+import {
+  getAnimeDescription,
+  getAnimeImgLink,
+  getAnimeRank,
+  getAnimeStatus,
+  getAnimeTitle,
+  getAnimeType,
+} from './animeGetters'
 
 const CACHE_DAYS = 1
 
@@ -18,14 +25,18 @@ export async function scrapeFullAnimeInfo(animeId: string): Promise<AnimeWithout
   const imageLink = getAnimeImgLink(document)
   const title = getAnimeTitle(document)
   const status = getAnimeStatus(document)
-  const otherTitles = [...document.querySelectorAll('.TxtAlt')].map(t => t.textContent?.trim()).filter(Boolean) as string[]
+  const otherTitles = [...document.querySelectorAll('.TxtAlt')]
+    .map(t => t.textContent?.trim())
+    .filter(Boolean) as string[]
 
   const description = getAnimeDescription(document)
   const rank = getAnimeRank(document, '.vtprmd')
-  const genres = [...document.querySelectorAll('.Nvgnrs a')].map(genre => genre.textContent?.trim()).filter(Boolean) as string[]
+  const genres = [...document.querySelectorAll('.Nvgnrs a')]
+    .map(genre => genre.textContent?.trim())
+    .filter(Boolean) as string[]
 
   const images: AnimeImages = {
-    coverImage: (await getOptimizeImage(imageLink, animeId ?? 'unknow')) ?? '',
+    coverImage: (await getOptimizeImage(imageLink, animeId ?? 'unknow')) ?? imageLink,
     carouselImages: await getCarouselImages(title),
   }
 
