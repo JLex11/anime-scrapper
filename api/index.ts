48648ca63b97ad4f36798c55e5764e3d47a0fcba
Lexx-Dev <alexcalle.paola@gmail.com>
2023-08-21 10:52:12 -0500

    export app for compatibility problem with vercel

--- api/index.ts
+++ api/index.ts
@@ -41,3 +41,4 @@ app.get(`/api${endPoints.IMAGES}`, async (req, res) => {
 app.use('*', (_, res) => res.status(404).send('Not found'))
 
 //app.listen(PORT, () => console.log(`🚀 Server running on http://localhost:${PORT}`))
+export default app
