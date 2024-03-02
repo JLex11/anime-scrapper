043127cf81dc81ac79b1452e59abc1940f4c4e21
Jlex11 <chikyelfurioso2011@gmail.com>
2024-03-01 23:33:55 -0500

    Fix import statement in scrapeAllAnimes.ts

--- src/scrapers/animes/scrapeAllAnimes.ts
+++ src/scrapers/animes/scrapeAllAnimes.ts
@@ -1,12 +1,12 @@
 import { JSDOM } from 'jsdom'
 import { getAnimeInfo } from '../../../api/controllers/animes/getAnimeInfo'
 import { animeFLVPages } from '../../enums'
-import { Anime } from '../../types'
+import { type Anime } from '../../types'
 import { getFulfilledResults } from '../../utils/getFulfilledResults'
 import { getAnimeIdFromLink, getAnimeOriginalLink } from './animeGetters'
 
 export async function scrapeAllAnimes(page: number = 1): Promise<Anime[]> {
-  const html = await fetch(`${animeFLVPages.BASE}/browse?page=${page}`).then(res => res.text())
+  const html = await fetch(`${animeFLVPages.BASE}/browse?page=${page}`).then(async res => await res.text())
 
   const { document } = new JSDOM(html).window
 
