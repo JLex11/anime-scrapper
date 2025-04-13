6ffc8f41e40824cb69d4c7c135fa64771fad7794
Lexx <chikyelfurioso2011@gmail.com>
2025-04-13 13:42:56 -0500

    Agregar enrutador para manejar la recuperación de imágenes desde Cloudflare R2

--- api/index.ts
+++ api/index.ts
@@ -3,9 +3,9 @@ import express from 'express'
 import morgan from 'morgan'
 import { setOriginPath } from '../src/config'
 import { endPoints } from '../src/enums'
-import { s3GetOperation } from '../src/services/cloudflareR2'
 import animesRouter from './router/animes'
 import episodesRouter from './router/episodes'
+import imagesRouter from './router/images'
 import routesDocumentation from './router/routesDocumentation'
 
 const app: express.Application = express()
@@ -26,23 +26,7 @@ app.get('/', (_, res) => res.redirect('/api'))
 app.use('/api/', routesDocumentation)
 app.use('/api/animes', animesRouter)
 app.use('/api/episodes', episodesRouter)
-
-app.get(`/api${endPoints.IMAGES}`, async (req, res) => {
-	const { imgFilename } = req.params
-
-	try {
-		const s3Response = await s3GetOperation({ filename: imgFilename })
-		const imgBuffer = s3Response?.Body
-
-		res.setHeader('Content-Type', 'image/webp')
-		res.setHeader('Cache-Control', 'public, max-age=31536000, immutable')
-
-		res.send(imgBuffer)
-	} catch (error) {
-		console.error(error)
-		res.status(404).send({ error: 'Image not found' })
-	}
-})
+app.use(`/api${endPoints.IMAGES}`, imagesRouter)
 
 app.use('*', (_, res) => {
 	res.status(404).send('Not found')
---
+++ api/router/images.ts
@@ -0,0 +1,24 @@
+import { Router as RouterApp } from 'express'
+import { endPoints } from '../../src/enums'
+import { s3GetOperation } from '../../src/services/cloudflareR2'
+
+const router = RouterApp()
+
+router.get(`/api${endPoints.IMAGES}`, async (req, res) => {
+	const { imgFilename } = req.params
+
+	try {
+		const s3Response = await s3GetOperation({ filename: imgFilename })
+		const imgBuffer = s3Response?.Body
+
+		res.setHeader('Content-Type', 'image/webp')
+		res.setHeader('Cache-Control', 'public, max-age=31536000, immutable')
+
+		res.send(imgBuffer)
+	} catch (error) {
+		console.error(error)
+		res.status(404).send({ error: 'Image not found' })
+	}
+})
+
+export default router
