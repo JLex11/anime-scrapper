d17d4de96a477c0c70d50eb62802e9e6e9a61b56
Lexx <chikyelfurioso2011@gmail.com>
2025-04-13 13:39:52 -0500

    Reemplazar la función mapAnimeImagesURLs por mapAnimeImages en searchAnimes para unificar el mapeo de imágenes de anime.

--- api/controllers/animes/searchAnimes.ts
+++ api/controllers/animes/searchAnimes.ts
@@ -1,6 +1,6 @@
 import { getAnimesByQuery } from '../../../src/services/database/animes'
 import type { Anime } from '../../../src/types'
-import { mapAnimeImagesURLs } from './getAnimeInfo'
+import { mapAnimeImages } from './getAnimeInfo'
 
 export const searchAnimes = async (query: string, page?: number, pageSize?: number): Promise<Anime[] | undefined> => {
 	const animesResponse = await getAnimesByQuery(query, page, pageSize)
@@ -9,6 +9,6 @@ export const searchAnimes = async (query: string, page?: number, pageSize?: numb
 	if (!animes) return
 
 	return animes.map(anime => {
-		return { ...anime, images: mapAnimeImagesURLs(anime.images) }
+		return mapAnimeImages(anime)
 	})
 }
