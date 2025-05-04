8aa27abd17fb67ba2a08f6af88f4c851011eaebe
Lexx <chikyelfurioso2011@gmail.com>
2025-05-04 13:35:21 -0500

    refactor: eliminar verificación de actualización en getAnimeInfo

--- api/controllers/animes/getAnimeInfo.ts
+++ api/controllers/animes/getAnimeInfo.ts
@@ -2,7 +2,6 @@ import { domainsToFilter } from '../../../src/constants'
 import { scrapeFullAnimeInfo } from '../../../src/scrapers/animes/scrapeFullAnimeInfo'
 import { UpsertAnimes, getAnimeBy } from '../../../src/services/database/animes'
 import type { Anime } from '../../../src/types'
-import { isUpToDate } from '../../../src/utils/isUpToDate'
 import { mapOriginPath } from '../../../src/utils/mapOriginPath'
 
 export const mapAnimeImages = (anime: Anime) => {
@@ -29,12 +28,9 @@ const getCurrentTime = () => new Date().toISOString()
 export const getAnimeInfo = async (animeId: string): Promise<Anime | null> => {
 	const { data: animeFromDb } = await getAnimeBy('animeId', animeId)
 
-	const isUpdated = !animeFromDb || !isUpToDate(animeFromDb.updated_at)
 	const hasCarouselImages = Boolean(animeFromDb?.images?.carouselImages?.length)
 
-	if (!isUpdated && hasCarouselImages) {
-		return mapAnimeImages(animeFromDb)
-	}
+	if (animeFromDb && hasCarouselImages) return mapAnimeImages(animeFromDb)
 
 	const currentTime = getCurrentTime()
 
