03e9276e4b4d7b332f9be73e463139e85f30ef2b
JLex11 <chikyelfurioso2011@gmail.com>
2026-03-24 20:47:36 -0500

    chore: add api request collection

--- docs/.astro/content-modules.mjs
+++ docs/.astro/content-modules.mjs
@@ -1,6 +1,17 @@
 
 export default new Map([
-["src/content/docs/animes/lista-de-animes.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fdocs%2Fanimes%2Flista-de-animes.mdx&astroContentModuleFlag=true")],
-["src/content/docs/introduccion.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fdocs%2Fintroduccion.mdx&astroContentModuleFlag=true")],
-["src/content/docs/episodios/ultimos-episodios.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fdocs%2Fepisodios%2Fultimos-episodios.mdx&astroContentModuleFlag=true")]]);
+["src/content/docs/introduction.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fdocs%2Fintroduction.mdx&astroContentModuleFlag=true")],
+["src/content/docs/animes/details.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fdocs%2Fanimes%2Fdetails.mdx&astroContentModuleFlag=true")],
+["src/content/docs/animes/broadcast.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fdocs%2Fanimes%2Fbroadcast.mdx&astroContentModuleFlag=true")],
+["src/content/docs/animes/latest.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fdocs%2Fanimes%2Flatest.mdx&astroContentModuleFlag=true")],
+["src/content/docs/animes/episodes.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fdocs%2Fanimes%2Fepisodes.mdx&astroContentModuleFlag=true")],
+["src/content/docs/animes/list.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fdocs%2Fanimes%2Flist.mdx&astroContentModuleFlag=true")],
+["src/content/docs/animes/rating.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fdocs%2Fanimes%2Frating.mdx&astroContentModuleFlag=true")],
+["src/content/docs/animes/related.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fdocs%2Fanimes%2Frelated.mdx&astroContentModuleFlag=true")],
+["src/content/docs/animes/search.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fdocs%2Fanimes%2Fsearch.mdx&astroContentModuleFlag=true")],
+["src/content/docs/episodes/details.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fdocs%2Fepisodes%2Fdetails.mdx&astroContentModuleFlag=true")],
+["src/content/docs/episodes/latest.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fdocs%2Fepisodes%2Flatest.mdx&astroContentModuleFlag=true")],
+["src/content/docs/episodes/sources.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fdocs%2Fepisodes%2Fsources.mdx&astroContentModuleFlag=true")],
+["src/content/docs/images/optimized.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fdocs%2Fimages%2Foptimized.mdx&astroContentModuleFlag=true")],
+["src/content/docs/routes/api-routes.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fdocs%2Froutes%2Fapi-routes.mdx&astroContentModuleFlag=true")]]);
 		
